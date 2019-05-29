a1a4accb0e2645736c2cada97669e32bb809028a
acskang <acskang@gmail.com>
2019-05-29 15:44:51 +0900

    add live chat

--- madie/settings.py
+++ madie/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
-    'chatterbot.ext.django_chatterbot',
+    # 'chatterbot.ext.django_chatterbot',
     'widget_tweaks',
     'boards',
     'accounts',
